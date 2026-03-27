ff5e4fcb3c166c120ad5585d9e3b5d7d1bf6d8b8
ElianYLV <elianarturociro@gmail.com>
2026-03-27 12:55:54 -0600

    commit9

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,27 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// prueba conexión DB
 pool.query('SELECT current_database()', (err, res) => {
   if (err) {
-    console.error('Error obteniendo base de datos:', err);
+    console.error('Error DB:', err);
   } else {
     console.log('BASE:', res.rows);
   }
 });
 
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-
-app.use(cors());
-
-app.use(express.json());
-app.use('/api/pokemongen1', require('./routes/pokemongen1Routes'));
 app.use('/api/pokemon', require('./routes/pokemonRoutes'));
+app.use('/api/pokemongen1', require('./routes/pokemongen1Routes'));
 app.use('/api/usuarios', require('./routes/userRoutes'));
 
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
